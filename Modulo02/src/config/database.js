89324060dde23695bbd0558b7f434493458458ee
Giuliano <giulianocrivelli@hotmail.com>
2019-07-01 01:30:05 +0000

    added eslint, prettier, sequelize and configs to database

---
+++ Modulo02/src/config/database.js
@@ -0,0 +1,10 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+  },
+};
