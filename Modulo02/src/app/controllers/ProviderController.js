9f95a18b8b80ffa50a3587d8fba7e2dda2aaf6c2
Giuliano <giulianocrivelli@hotmail.com>
2019-07-04 20:29:56 +0000

    added providers list

--- Modulo02/src/app/controllers/ProviderController.js
+++ Modulo02/src/app/controllers/ProviderController.js
@@ -5,12 +5,12 @@ class ProviderController {
   async index(req, res) {
     const providers = await User.findAll({
       where: { provider: true },
-      attributes: ['id', 'name', 'emai', 'avatar_id'],
+      attributes: ['id', 'name', 'email', 'avatar_id'],
       include: [
         {
           model: File,
           as: 'avatar',
-          attributes: ['name', 'path'],
+          attributes: ['name', 'path', 'url'],
         },
       ],
     });
