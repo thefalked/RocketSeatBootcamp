99cd4cb4bf25ed53da7b911f5f5468e434207e83
Giuliano <giulianocrivelli@hotmail.com>
2019-07-09 04:25:32 +0000

    create condition to not be able to delete an appointment already deleted

--- Modulo02/src/app/controllers/AppointmentController.js
+++ Modulo02/src/app/controllers/AppointmentController.js
@@ -135,6 +135,12 @@ class AppointmentController {
       ],
     });
 
+    if (appointment.canceled_at) {
+      return res.status(401).json({
+        error: 'This appointment already was canceled.',
+      });
+    }
+
     if (appointment.user_id !== req.userId) {
       return res.status(401).json({
         error: "You don't have permission to cancel this appointment.",
