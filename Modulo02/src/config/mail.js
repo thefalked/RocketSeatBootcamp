1dba9f924cb1366b369c945d08486ff13c71a29f
Giuliano <giulianocrivelli@hotmail.com>
2019-07-09 02:36:01 +0000

    configured nodemailer

---
+++ Modulo02/src/config/mail.js
@@ -0,0 +1,24 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'f110bf130567dc',
+    pass: '15fd92f71c747a',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+/**
+ * === PRO ===
+ * Amazon SES
+ * Mailgun
+ * Sparkpost
+ * Mandril (Mailchimp)
+ * GMail
+ *
+ * === DEV ===
+ * Mailtrap
+ */
