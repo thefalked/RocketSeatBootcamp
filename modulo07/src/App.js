e1bac5c87ae4e834a543bcf0e68de59142bd6ce4
Giuliano <giulianocrivelli@hotmail.com>
2019-08-25 21:06:48 -0400

    modulo07 using hooks

---
+++ modulo07/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [tech, setTech] = useState(['ReactJS', 'React-Native']);
+  const [newTech, setNewTech] = useState('');
+
+  function handleAdd() {
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }
+
+  return (
+    <>
+      <ul>
+        {tech.map(t => (
+          <li key={t}>{t}</li>
+        ))}
+      </ul>
+      <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
+  );
+}
+
+export default App;
